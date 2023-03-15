3bc138c4c3f0ec7ad768fd55eb970a244a0c849a
Pratish Balaji Thormothe <118709592+Pratishbunty@users.noreply.github.com>
2023-03-15 14:22:55 +0530

    Add files via upload

---
+++ ds6.cpp
@@ -0,0 +1,31 @@
+//C++ implementation 
+#include<bits/stdc++.h>
+using namespace std;
+
+//function to return the count
+int count_greater ( int a r r [], int n)
+{
+	int min = INT_MAX;
+	int counter = 0;
+	
+	// Comparing the given element 
+	// with minimum element till
+	// occurred till now.
+	for ( int = n-1; i>=0;i--){
+		if ( a r r [i]>min) {
+			counter ==;
+			
+		}
+	}
+    return counter;
+    
+    Driver code
+       main ()
+       
+       int a r r []={ 7, 4, 8, 2, 9};
+       int n = size of ( a r r ) / size of ( int );
+       
+       cout << count-greater ( a r r,n) <<end1;
+       return 0;
+   }
+   
